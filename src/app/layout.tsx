aee27503b03a6f4eee884245fc0d6ff592d21b64
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-04-03 05:00:01 +0900

    chore: chore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import RecoilRootWrapper from '@/components/RecoilWrapper.tsx';
+import RecoilRootWrapper from '@/recoil/RecoilWrapper.tsx';
 
 const inter = Inter({ subsets: ['latin'] });
 
