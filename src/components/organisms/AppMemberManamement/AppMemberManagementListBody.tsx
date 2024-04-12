ca5fed09a5b9efe4aaa8a3eb0939ef0d3bd4cac2
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-04-12 18:13:46 +0900

    feat: 앱 회원 관리 template

---
+++ src/components/organisms/AppMemberManamement/AppMemberManagementListBody.tsx
@@ -0,0 +1,60 @@
+import Checkbox from '@/components/atoms/AllBtn/Checkbox/Checkbox';
+import React from 'react';
+
+export type Props = {
+  index: string;
+  name: string;
+  studentId: string;
+  phoneNumber: string;
+  personalAgree: boolean;
+  thirdAgree: boolean;
+  plus: string;
+  minus: string;
+  creationDate: string;
+};
+
+const AppMemberManagementListBody = ({
+  index,
+  name,
+  studentId,
+  phoneNumber,
+  personalAgree,
+  thirdAgree,
+  plus,
+  minus,
+  creationDate,
+}: Props) => {
+  return (
+    <>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale30'>{index}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{name}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{studentId}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{phoneNumber}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{personalAgree ? 'O' : 'X'}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{thirdAgree ? 'O' : 'X'}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{plus}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{minus}</h1>
+      </td>
+      <td className='text-center'>
+        <h1 className='H4-caption text-gray-grayscale50'>{creationDate}</h1>
+      </td>
+    </>
+  );
+};
+
+export default AppMemberManagementListBody;
