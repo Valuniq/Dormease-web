096d7c8389536a4c539ecf8588ab1ec69f7a115d
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-04-12 20:59:21 +0900

    feat: login template

---
+++ src/app/Login/page.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image';
+import React from 'react';
+import loginLogo from '@public/images/LoginLogo.png';
+import LoginInputText from '@/components/atoms/InputText/LoginInputText/LoginInputText';
+import LoginBtn from '@/components/atoms/AllBtn/LoginBtn/LoginBtn';
+
+const page = () => {
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <Image src={loginLogo} width={372} height={136.5} alt='login' className='object-contain mb-43' />
+      <div className='w-510 h-426 bg-white rounded-[12.35px] shadow-lg flex flex-col items-center justify-center'>
+        <h1 className='H1 text-gray-grayscale50 mb-31'>도미즈 관리자 로그인</h1>
+        <div className='mb-19'>
+          <LoginInputText
+            input={''}
+            setInput={function (id: string): void {
+              throw new Error('Function not implemented.');
+            }}
+            placeholder={'아이디'}
+            type={'text'}
+          />
+        </div>
+        <div className='mb-61'>
+          <LoginInputText
+            input={''}
+            setInput={function (id: string): void {
+              throw new Error('Function not implemented.');
+            }}
+            placeholder={'비밀번호'}
+            type={'password'}
+          />
+        </div>{' '}
+        <LoginBtn label={'로그인'} disabled={false} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/atoms/InputText/LoginInputText/LoginInputText.tsx
+++ src/components/atoms/InputText/LoginInputText/LoginInputText.tsx
@@ -2,9 +2,10 @@ type Props = {
   input: string;
   setInput: (id: string) => void;
   placeholder: string;
+  type: 'text' | 'password';
 };
 
-const LoginInputText = ({ input, setInput, placeholder }: Props) => {
+const LoginInputText = ({ input, setInput, placeholder, type }: Props) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
@@ -13,7 +14,7 @@ const LoginInputText = ({ input, setInput, placeholder }: Props) => {
     <input
       className={`${input ? 'border-gray-grayscale50' : 'border-gray-grayscale30'} H2  border-[1.5px]  outline-none focus:border-gray-grayscale30 px-21 w-414 h-59 rounded-20 text-gray-grayscale50 placeholder:text-gray-grayscale30`}
       placeholder={placeholder}
-      type='text'
+      type={type}
       value={input}
       onChange={handleChange}
     />
