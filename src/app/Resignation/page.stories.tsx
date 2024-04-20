984738736c2cb3be6d82886b008142161a036375
rlotr02 <0314kelee@naver.com>
2024-04-20 18:04:50 +0900

    feat: resignation page template

---
+++ src/app/Resignation/page.stories.tsx
@@ -0,0 +1,174 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import page from './page';
+
+const meta: Meta<typeof page> = {
+  title: 'app/Resignation/page',
+  component: page,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof page>;
+
+const list = [
+  {
+    name: '김김김김',
+    schoolNumber: '99999998',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: false,
+    submissionDate: '24.00.00',
+    depositRefund: true,
+    isChecked: true,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+  {
+    name: '김김김김',
+    schoolNumber: '99999999',
+    building: '명덕관(4인실)',
+    roomNumber: '999호',
+    exitDate: '24.00.00',
+    hasKey: true,
+    submissionDate: '24.00.00',
+    depositRefund: false,
+    isChecked: false,
+  },
+];
+
+export const Primary: Story = {
+  args: {
+    clickSchoolNumber: '99999998',
+    list: list,
+  },
+};
+
+export const NoneList: Story = {
+  args: {
+    clickSchoolNumber: '99999998',
+    list: [],
+  },
+};
---
+++ src/components/organisms/Resignation/ResignationList.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import ResignationListBody from './ResignationListBody';
+import Checkbox from '@/components/atoms/AllBtn/Checkbox/Checkbox';
+import NoneList from '../NoneList/NoneList';
+
+type Props = {
+  onStudentClick: (schoolNumber: string) => void;
+  setIsChecked: (isChecked: boolean) => void;
+  isAllChecked: boolean;
+  setIsAllChecked: (isChecked: boolean) => void;
+  list: {
+    name: string;
+    schoolNumber: string;
+    building: string;
+    roomNumber: string;
+    exitDate: string;
+    hasKey: boolean;
+    submissionDate: string;
+    depositRefund: boolean;
+    isChecked: boolean;
+  }[];
+};
+
+const ResignationList = ({ list, onStudentClick, setIsChecked, isAllChecked, setIsAllChecked }: Props) => {
+  return (
+    <table className='text-nowrap text-center text-gray-grayscale50'>
+      <thead className='table w-[1200px]'>
+        <tr>
+          <th className='H4 w-[10%]'>이 름</th>
+          <th className='H4 w-[12%]'>학 번</th>
+          <th className='H4 w-[17%]'>건 물</th>
+          <th className='H4 w-[9%]'>호 실</th>
+          <th className='H4 w-[12%]'>퇴실날짜</th>
+          <th className='H4 w-[10%]'>열쇠 수령</th>
+          <th className='H4 w-[12%]'>제출날짜</th>
+          <th className='H4 w-[10%]'>보증금 환급</th>
+          <th className={`H4 w-[8%] ${list && list.length > 0 ? 'visible' : 'invisible'}`}>
+            <div className='flex items-center justify-center text-center w-full gap-6'>
+              전 체
+              <Checkbox isChecked={isAllChecked} setIsChecked={setIsAllChecked} />
+            </div>
+          </th>
+        </tr>
+        <tr className='h-15 border-b-1' />
+      </thead>
+      <tbody className='block h-677 overflow-y-auto scrollbar-table'>
+        {list && list.length > 0 ? (
+          <>
+            <tr className='h-15' />
+            {list.map((data, index) => {
+              return (
+                <ResignationListBody
+                  key={index}
+                  name={data.name}
+                  schoolNumber={data.schoolNumber}
+                  building={data.building}
+                  roomNumber={data.roomNumber}
+                  exitDate={data.exitDate}
+                  hasKey={data.hasKey}
+                  submissionDate={data.submissionDate}
+                  depositRefund={data.depositRefund}
+                  isChecked={data.isChecked}
+                  setIsChecked={setIsChecked}
+                  onStudentClick={onStudentClick}
+                />
+              );
+            })}
+          </>
+        ) : (
+          <NoneList />
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default ResignationList;
