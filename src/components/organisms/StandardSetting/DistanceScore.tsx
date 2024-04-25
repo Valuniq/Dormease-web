d547b1a4d8ad1fc263a22d110c95ac80e7862c5a
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-04-26 06:16:02 +0900

    feat: 거리 기준 점수 표 생성

---
+++ src/components/organisms/StandardSetting/DistanceScore.tsx
@@ -0,0 +1,29 @@
+import MediumInputText from '@/components/atoms/InputText/MediumInputText/MediumInputText';
+import React from 'react';
+
+const DistanceScore = () => {
+  const scores = Array.from({ length: 10 }, (_, index) => index * 0.5);
+  return (
+    <table className=' border-1 border-gray-grayscale50'>
+      <tbody className='H4 text-gray-grayscale50 '>
+        {scores.map((score, index, array) => (
+          <tr
+            className={` ${index === array.length - 1 ? '' : ' border-b-[1.5px] border-b-gray-grayscale20'} h-41`}
+            key={index}
+          >
+            <td className='w-217 text-center border-r-[0.5px] border-r-gray-grayscale20'>{score}점</td>
+            <td className='w-333 text-center'>
+              <MediumInputText
+                placeholder={'거리 점수 사유를 입력해주세요.'}
+                input={''}
+                setInput={(input: string) => {}}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default DistanceScore;
