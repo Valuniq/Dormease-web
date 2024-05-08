e88c585eb338807ccf6755079fff4868c1344ba0
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-05-08 12:08:24 +0900

    design: menu bar selected 디자인 변경

--- src/components/atoms/AllBtn/MenuBtn/MenuBtn.tsx
+++ src/components/atoms/AllBtn/MenuBtn/MenuBtn.tsx
@@ -9,13 +9,16 @@ type Props = {
 
 const MenuBtn = ({ label, menu, selected = false, ...props }: Props & React.HtmlHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button
-      {...props}
-      className={`alert-cap flex items-center w-240 h-48 rounded-8 text-gray-grayscale-50 ${selected ? 'bg-gray-grayscale20' : 'bg-transparent hover:bg-gray-grayscale10 hover:hover-transition active:bg-gray-grayscale20'}`}
-    >
-      <Image className='ml-19 mr-11' src={menu} alt='Menu' width={24} height={24} />
-      {label}
-    </button>
+    <div className='flex'>
+      <button
+        {...props}
+        className={`alert-cap flex items-center h-48 rounded-8 text-gray-grayscale-50 ${selected ? 'w-222 bg-blue-blue15 mr-12' : ' w-240 bg-transparent hover:bg-gray-grayscale10 hover:hover-transition active:bg-gray-grayscale20'}`}
+      >
+        <Image className='ml-19 mr-11' src={menu} alt='Menu' width={24} height={24} />
+        {label}
+      </button>
+      <div className={`hover-transition ${selected ? 'w-6 h-48 rounded-8 bg-blue-blue30' : 'hidden'}`} />
+    </div>
   );
 };
 
