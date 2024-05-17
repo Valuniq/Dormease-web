f5ba2195e1fa71e78fd41cf3e72c51d327b1c92b
rlotr02 <0314kelee@naver.com>
2024-05-18 00:40:21 +0900

    Feat: edit commit

--- src/apis/Withdrawal.ts
+++ src/apis/Withdrawal.ts
@@ -1,10 +1,10 @@
 import { withdrawalLists } from '@/types/withdrawal';
 
 const accessToken =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaG9uaWw6VVNFUiIsImlzcyI6IkRvcm1lYXNlVmFsdW5pUSIsImlhdCI6MTcxNTQ1MTgyNSwiZXhwIjoxNzE1NDUzNjI1fQ.lcLyflbO6CnAqGvyeJxK3_Ntl9_LdIAjdASKdcD40MRyDtk-F2_zQt_raXnVGn0dTNaaK97_2H2OxEXqtnzOEg';
+  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaG9uaWw6VVNFUiIsImlzcyI6IkRvcm1lYXNlVmFsdW5pUSIsImlhdCI6MTcxNTk2MDI1OCwiZXhwIjoxNzE1OTYyMDU4fQ.mqaEjf0Z698RoxpLo0eVPJ7PVh8EJTKRKhjX43yxnTIRtpjwEZ7vObDKjXK1hWMLeE0l5ULJ-YVaksKaw6v5mQ';
 
 export const getWithdrawalLists = async (page: number): Promise<withdrawalLists> => {
-  const res = await fetch(`/api/v1/web/users/management/delete?page=${page}`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/web/users/management/delete?page=${page}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -22,13 +22,16 @@ export const getWithdrawalLists = async (page: number): Promise<withdrawalLists>
 };
 
 export const getWithdrawalSearch = async (page: number, keyword: string): Promise<withdrawalLists> => {
-  const res = await fetch(`/api/v1/web/users/management/delete/search?page=${page}&keyword=${keyword}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `bearer ${accessToken}`,
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/web/users/management/delete/search?page=${page}&keyword=${keyword}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${accessToken}`,
+      },
     },
-  });
+  );
 
   if (!res.ok) {
     throw new Error(`Server responded with status ${res.status}`);
