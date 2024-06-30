5684bb8037cfeccec71f02a8331000a4d844f04a
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-01 01:57:16 +0900

    refactor: point management fetch to swr

--- src/apis/PointManagment.ts
+++ src/apis/PointManagment.ts
@@ -1,96 +1,43 @@
+// src/apis/pointManagement.ts
 import { BASE_URL } from '@/constants/path';
 import { PageDetailInfo, PointListResponse, PointMemberResponse, ResidentPointResponse } from '@/types/pointManagement';
-import fetchWithToken from '@/utils/fetchWithToken';
+import useSWR from 'swr';
+import swrWithToken from '@/utils/swrWithToken';
 
 // * 사생 목록 조회
-export const getPointMemberList = async (page: number): Promise<PointMemberResponse> => {
-  const res = await fetchWithToken(`${BASE_URL}/api/v1/web/points?page=${page}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (!res.ok) {
-    console.log('Response:', res);
-    throw new Error('Failed');
-  }
-  const response = await res.json();
-  return response as PointMemberResponse;
+export const usePointMemberList = (page: number) => {
+  const { data, error } = useSWR<PointMemberResponse>(`${BASE_URL}/api/v1/web/points?page=${page}`, swrWithToken);
+  return { data, error, isLoading: !error && !data };
 };
 
 // * 상/벌점 리스트 조회
-export const getPointsDetail = async (): Promise<PointListResponse> => {
-  const res = await fetchWithToken(`${BASE_URL}/api/v1/web/points/detail`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  if (!res.ok) {
-    console.log(res);
-    throw new Error('Failed');
-  }
-  const response = await res.json();
-  return response as PointListResponse;
+export const usePointsDetail = () => {
+  const { data, error } = useSWR<PointListResponse>(`${BASE_URL}/api/v1/web/points/detail`, swrWithToken);
+  return { data, error, isLoading: !error && !data };
 };
 
 // * 상/벌점 리스트 삭제
 export const deletePointsDetail = async (pointId: number) => {
-  const res = await fetchWithToken(`${BASE_URL}/api/v1/web/points/detail/${pointId}`, {
+  const res = await swrWithToken(`${BASE_URL}/api/v1/web/points/detail/${pointId}`, {
     method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
   });
-  if (!res.ok) {
-    console.log(res);
-    throw new Error('Failed');
-  }
-  const response = await res.json();
-  return response;
+  return res;
 };
 
 // * 사생 상벌점 부여
 export const postMemberPoint = async (residentId: number, pointType: string, points: { pointId: number }[]) => {
-  const res = await fetchWithToken(`${BASE_URL}/api/v1/web/points/${residentId}?pointType=${pointType}`, {
+  const res = await swrWithToken(`${BASE_URL}/api/v1/web/points/${residentId}?pointType=${pointType}`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify(points),
   });
-
-  if (!res.ok) {
-    const errorData = await res.json();
-    console.error('[Error response in postMemberPoint]', errorData);
-    throw new Error('Failed to assign points');
-  }
-
-  const response = await res.json();
-  return response;
+  return res;
 };
 
 // * 사생 상/벌점 내역 상세 조회
-export const getPointsByResidentId = async ({
-  residentId,
-  page,
-}: {
-  residentId: number;
-  page: PageDetailInfo;
-}): Promise<ResidentPointResponse> => {
-  const res = await fetchWithToken(`${BASE_URL}/api/v1/web/points/${residentId}?page=${page.currentPage}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-
-  if (!res.ok) {
-    const errorData = await res.json();
-    console.error('[Error response in getPointsByResidentId]', errorData);
-    throw new Error('Failed to assign points');
-  }
-
-  const response = await res.json();
-  return response as ResidentPointResponse;
+export const usePointsByResidentId = (residentId: number, page: PageDetailInfo) => {
+  const { data, error } = useSWR<ResidentPointResponse>(
+    `${BASE_URL}/api/v1/web/points/${residentId}?page=${page.currentPage}`,
+    swrWithToken,
+  );
+  return { data, error, isLoading: !error && !data };
 };
