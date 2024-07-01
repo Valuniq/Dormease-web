8a8f9a7084759ef0b647447cceac6c1bd078d6c0
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-02 03:52:08 +0900

    feat: notice 조회

--- src/components/organisms/Notice/NoticeList/NoticeListBody.tsx
+++ src/components/organisms/Notice/NoticeList/NoticeListBody.tsx
@@ -1,30 +1,31 @@
-import { noticeList } from '@/types/notice';
+'use client';
+import { noticeResponseDataList } from '@/types/notice';
 import pinIcon from '@public/images/pinIcon.png';
 import Image from 'next/image';
 
-const NoticeListBody = ({ index, title, writer, registrationDate, isExistedFile, views, isPinned }: noticeList) => {
+const NoticeListBody = ({ notificationId, pinned, title, writer, createdDate, existFile }: noticeResponseDataList) => {
   const slicedTitle = title.length > 35 ? title.slice(0, 35) + '...' : title;
   return (
-    <>
-      <tr
-        className={`${isPinned ? 'bg-gray-grayscale10' : 'bg-white'} h-38 hover-transition hover:bg-gray-grayscale10 cursor-pointer active:bg-gray-grayscale20 H4-caption text-gray-grayscale50`}
-      >
-        <td className='text-center'>
-          {isPinned ? (
-            <div className='flex items-center justify-center'>
-              {index} <Image className=' object-contain ml-2 ' src={pinIcon} width={13} height={19.61} alt='pinIcon' />
-            </div>
-          ) : (
-            <> {index}</>
-          )}
-        </td>
-        <td className='text-center'>{slicedTitle}</td>
-        <td className='text-center'>{writer}</td>
-        <td className='text-center'>{registrationDate}</td>
-        <td className='text-center'>{isExistedFile ? 'O' : 'X'}</td>
-        <td className='text-center'>{views}</td>
-      </tr>
-    </>
+    <tr
+      key={notificationId}
+      className={`${pinned ? 'bg-gray-grayscale10' : 'bg-white'} h-38 hover-transition hover:bg-gray-grayscale10 cursor-pointer active:bg-gray-grayscale20 H4-caption text-gray-grayscale50`}
+    >
+      <td className='text-center'>
+        {pinned ? (
+          <div className='flex items-center justify-center'>
+            {notificationId}{' '}
+            <Image className=' object-contain ml-2 ' src={pinIcon} width={13} height={19.61} alt='pinIcon' />
+          </div>
+        ) : (
+          <> {notificationId}</>
+        )}
+      </td>
+      <td className='text-center'>{slicedTitle}</td>
+      <td className='text-center'>{writer}</td>
+      <td className='text-center'>{createdDate}</td>
+      <td className='text-center'>{existFile ? 'O' : 'X'}</td>
+      <td className='text-center'>조회수</td>
+    </tr>
   );
 };
 
