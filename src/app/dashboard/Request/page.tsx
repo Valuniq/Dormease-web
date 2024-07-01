7d6cebe8795a45d6f8f387414a9ad587906bb581
rlotr02 <0314kelee@naver.com>
2024-07-02 02:23:52 +0900

    fix: 빌드 에러 수정

--- src/app/dashboard/Request/page.tsx
+++ src/app/dashboard/Request/page.tsx
@@ -2,22 +2,11 @@
 import RequestList from '@/components/organisms/Request/RequestList';
 import React from 'react';
 
-type Props = {
-  onRequestClick: (index: number) => void;
-  list: {
-    index: number;
-    title: string;
-    name: string;
-    date: string;
-    progression: string;
-  }[];
-};
-
-const Request = ({ onRequestClick, list }: Props) => {
+const Request = () => {
   return (
     <div className='flex flex-col w-[1200px]'>
       <h1 className='text-center H0 text-gray-grayscale50 text-nowrap'>요청사항</h1>
-      <RequestList list={list} onRequestClick={onRequestClick} />
+      <RequestList list={[]} onRequestClick={(index: number) => index} />
     </div>
   );
 };
