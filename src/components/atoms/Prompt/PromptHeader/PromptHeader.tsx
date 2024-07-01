093c48c51c9a56f51873ff0b44129120c3346d19
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-02 06:47:50 +0900

    hotfix: build error 이미지 파일명 수정

--- src/components/atoms/Prompt/PromptHeader/PromptHeader.tsx
+++ src/components/atoms/Prompt/PromptHeader/PromptHeader.tsx
@@ -1,17 +1,17 @@
 'use client';
 import Image from 'next/image';
-import PromptHeaderLogo from '@public/images/PromptHeaderLogo.png';
-import PromptHeaderOut from '@public/images/PromptHeaderOut.png';
+import PromptLogo from '@public/images/PromptLogo.png';
+import PromptOut from '@public/images/PromptOut.png';
 import React from 'react';
 
 const PromptHeader = ({ bgColor }: { bgColor?: string }) => {
   return (
     <div className={`${bgColor ? bgColor : 'bg-blue-blue30'} w-full h-39  flex justify-between pl-15 rounded-t-8`}>
-      <Image src={PromptHeaderLogo} alt='promptheaderlogo' className='object-contain' width={39} height={11.65} />
+      <Image src={PromptLogo} alt='PromptLogo' className='object-contain' width={39} height={11.65} />
       <button
         className={`hover:bg-red-red30 ${bgColor ? bgColor : 'bg-blue-blue30'} w-39 h-39 flex items-center justify-center rounded-tr-8 hover-transition`}
       >
-        <Image src={PromptHeaderOut} alt='promptheaderout' className='object-contain' width={10} height={10} />
+        <Image src={PromptOut} alt='PromptOut' className='object-contain' width={10} height={10} />
       </button>
     </div>
   );
--- src/components/organisms/Prompt/PointManagement/PromptHeader/PromptHeader.tsx
+++ src/components/organisms/Prompt/PointManagement/PromptHeader/PromptHeader.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
-import PromptHeaderLogo from '@public/images/PromptHeaderLogo.png';
-import PromptHeaderOut from '@public/images/PromptHeaderOut.png';
+import PromptLogo from '@public/images/PromptLogo.png';
+import PromptOut from '@public/images/PromptOut.png';
 import React from 'react';
 import usePointManagementModal from '@/hooks/usePointManagmentModal';
 import { TPointManagementModalState } from '@/recoil/pointManagement';
@@ -11,12 +11,12 @@ const PromptHeader = ({ bgColor, modalName }: { bgColor?: string; modalName: key
 
   return (
     <div className={`${bgColor ? bgColor : 'bg-blue-blue30'} w-full h-39  flex justify-between pl-15 rounded-t-8`}>
-      <Image src={PromptHeaderLogo} className='object-contain' width={39} height={11.65} alt='promptHeaderLogo' />
+      <Image src={PromptLogo} className='object-contain' width={39} height={11.65} alt='PromptLogo' />
       <button
         onClick={() => handleCloseModal(modalName)}
         className={`hover:bg-red-red30 ${bgColor ? bgColor : 'bg-blue-blue30'} w-39 h-39 flex items-center justify-center rounded-tr-8 hover-transition`}
       >
-        <Image src={PromptHeaderOut} className='object-contain' width={10} height={10} alt='promptHeaderOut' />
+        <Image src={PromptOut} className='object-contain' width={10} height={10} alt='PromptOut' />
       </button>
     </div>
   );
