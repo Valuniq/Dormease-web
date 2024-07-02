2f2d51502e194406fb8939d3010af370f5185fea
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-02 16:01:01 +0900

    fix: token 만료 시 처리
    수정 필요

--- src/utils/fetchWithToken.ts
+++ src/utils/fetchWithToken.ts
@@ -1,10 +1,14 @@
-// src/utils/fetchWithToken.ts
+import { BASE_URL } from '@/constants/path';
 import tokenManager from '@/utils/tokenManager';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants/tokenKey';
+import { useRouter } from 'next/router';
 
 const fetchWithToken = async (url: string, options: RequestInit = {}) => {
+  const router = useRouter(); // next/router의 useRouter를 사용하여 리디렉션 처리
+
   const accessToken = tokenManager.getToken(ACCESS_TOKEN);
   if (!accessToken) {
+    router.push('/');
     throw new Error('No access token found');
   }
 
@@ -31,16 +35,31 @@ const fetchWithToken = async (url: string, options: RequestInit = {}) => {
           ...options,
           headers,
         });
+
+        if (retryResponse.status === 401) {
+          // 새로운 액세스 토큰도 만료된 경우
+          tokenManager.removeToken(ACCESS_TOKEN);
+          tokenManager.removeToken(REFRESH_TOKEN);
+          router.push('/');
+          alert('세션이 만료되었습니다. 다시 로그인해 주세요.');
+        }
+
         return retryResponse;
       }
     }
+    // 리프레시 토큰도 만료된 경우
+    tokenManager.removeToken(ACCESS_TOKEN);
+    tokenManager.removeToken(REFRESH_TOKEN);
+    router.push('/');
+    alert('세션이 만료되었습니다. 다시 로그인해 주세요.');
+    throw new Error('Refresh token expired');
   }
 
   return response;
 };
 
 const refreshAccessToken = async (refreshToken: string) => {
-  const response = await fetch('http://13.209.177.109:8080/api/v1/auth/refresh-token', {
+  const response = await fetch(`${BASE_URL}/api/v1/auth/refresh-token`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
