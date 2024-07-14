82eb756107eb9c0cba9676193d7c6ebd2c089f17
rlotr02 <0314kelee@naver.com>
2024-07-14 22:22:58 +0900

    feat: recoil sessionStorage에 데이터 저장

--- src/recoil/request.ts
+++ src/recoil/request.ts
@@ -1,6 +1,16 @@
+import { recoilPersist } from 'recoil-persist';
+
 import { atom } from 'recoil';
+
+const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : undefined;
+
+export const { persistAtom } = recoilPersist({
+  key: 'requestIdState',
+  storage: sessionStorage,
+});
 
 export const requestIdState = atom<number>({
   key: 'requestIdState',
   default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
--- src/recoil/resignation.ts
+++ src/recoil/resignation.ts
@@ -1,6 +1,16 @@
+import { recoilPersist } from 'recoil-persist';
+
 import { atom } from 'recoil';
+
+const sessionStorage = typeof window !== 'undefined' ? window.sessionStorage : undefined;
+
+export const { persistAtom } = recoilPersist({
+  key: 'resignationIdState',
+  storage: sessionStorage,
+});
 
 export const resignationIdState = atom<number>({
   key: 'resignationIdState',
   default: 0,
+  effects_UNSTABLE: [persistAtom],
 });
