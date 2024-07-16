570f186c1abf3799d1742365abfe810ba9cc2e26
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-17 03:00:42 +0900

    feat: 전역 기능 상세 제한 파일 생성

---
+++ src/constants/restrictions.ts
@@ -0,0 +1,15 @@
+// Notice, FAQ
+export const RES_NOTIFICATIONS = {
+  title: {
+    maxLength: 50, // 제목의 최대 길이
+  },
+  description: {
+    maxLength: 2000, // 내용의 최대 길이
+  },
+  fileName: {
+    maxLength: 12,
+  },
+  file: {
+    maxCount: 5, // 파일 선택의 최대 개수
+  },
+};
