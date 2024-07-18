70fd85a698f74d00caf1e247f382b94bf22fc148
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-18 23:04:45 +0900

    fix: mediumInputText w-260 -> w-full 변경

--- src/components/atoms/InputText/MediumInputText/MediumInputText.tsx
+++ src/components/atoms/InputText/MediumInputText/MediumInputText.tsx
@@ -14,7 +14,7 @@ const MediumInputText = ({ placeholder, input, setInput }: Props) => {
 
   return (
     <input
-      className={`${input ? 'border-b-gray-grayscale50 ' : 'border-b-gray-grayscale20'} H4  focus:border-b-gray-grayscale50 w-260 h-26 border-b-1 placeholder:text-gray-grayscale20 focus:outline-none pb-6 text-gray-grayscale50 `}
+      className={`${input ? 'border-b-gray-grayscale50 ' : 'border-b-gray-grayscale20'} H4  focus:border-b-gray-grayscale50 w-full h-26 border-b-1 placeholder:text-gray-grayscale20 focus:outline-none pb-6 text-gray-grayscale50 `}
       placeholder={placeholder}
       type='text'
       value={input}
