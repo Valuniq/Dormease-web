1fbbfc7da1f4c3987d752526d4cb604b4be094ed
rlotr02 <0314kelee@naver.com>
2024-07-23 01:04:12 +0900

    design: UI 자잘한 수정

--- src/app/dashboard/JoinApplicationSetting/Resignation/Detail/page.tsx
+++ src/app/dashboard/JoinApplicationSetting/Resignation/Detail/page.tsx
@@ -41,15 +41,15 @@ const ResignationId = () => {
 
   return (
     <div className='flex flex-col text-center w-[1200px] gap-35'>
-      <h1 className='H0 text-gray-grayscale50 text-nowrap'>퇴사확인서 확인</h1>
+      <h1 className='H0 text-gray-grayscale50 text-nowrap w-[1200px]'>퇴사확인서 확인</h1>
       <div className='flex border-t-1 border-t-gray-grayscale50'>
         <div className='flex-1 flex flex-col gap-60 py-60 pl-103 pr-134'>
           <ResignationDetail label='이 름' value={detailData.residentName} />
-          <ResignationDetail label='학 과' value={detailData.major} />
-          <ResignationDetail label='학 번' value={detailData.studentNumber} />
-          <ResignationDetail label='학 년' value={detailData.schoolYear + '학년'} />
-          <ResignationDetail label='휴대전화' value={detailData.phoneNumber} />
-          <ResignationDetail label='건 물' value={detailData.dormitoryName} />
+          <ResignationDetail label='학 과' value={detailData.major ? detailData.major : '-'} />
+          <ResignationDetail label='학 번' value={detailData.studentNumber ? detailData.studentNumber : '-'} />
+          <ResignationDetail label='학 년' value={detailData.schoolYear ? detailData.schoolYear + '학년' : '-'} />
+          <ResignationDetail label='휴대전화' value={detailData.phoneNumber ? detailData.phoneNumber : '-'} />
+          <ResignationDetail label='건 물' value={detailData.dormitoryName ? detailData.dormitoryName : '-'} />
           <ResignationDetail
             label='보증금 환급 여부'
             value={
@@ -62,13 +62,16 @@ const ResignationId = () => {
           />
         </div>
         <div className='flex-1 flex flex-col gap-60 bg-gray-grayscale5 py-60 pl-119 pr-112'>
-          <ResignationDetail label='호 실' value={detailData.roomNumber + '호'} />
-          <ResignationDetail label='침대번호' value={detailData.bedNumber + '번'} />
+          <ResignationDetail label='호 실' value={detailData.roomNumber ? detailData.roomNumber + '호' : '-'} />
+          <ResignationDetail label='침대번호' value={detailData.bedNumber ? detailData.bedNumber + '번' : '-'} />
           <ResignationDetail label='열쇠 수령 여부' value={detailData.hasKey ? 'O' : '-'} />
           <ResignationDetail label='열쇠번호' value={detailData.keyNumber ? detailData.keyNumber : '-'} />
-          <ResignationDetail label='퇴실날짜' value={formatCreateDate(detailData.exitDate)} />
-          <ResignationDetail label='은행명' value={detailData.bankName} />
-          <ResignationDetail label='계좌번호' value={detailData.accountNumber} />
+          <ResignationDetail
+            label='퇴실날짜'
+            value={detailData.exitDate ? formatCreateDate(detailData.exitDate) : '-'}
+          />
+          <ResignationDetail label='은행명' value={detailData.bankName ? detailData.bankName : '-'} />
+          <ResignationDetail label='계좌번호' value={detailData.accountNumber ? detailData.accountNumber : '-'} />
         </div>
       </div>
       <div>
