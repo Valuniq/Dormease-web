16636b654df70d058928b90545be90dee4d1657b
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-22 19:37:28 +0900

    hotfix: token persis atom 사용

--- src/recoil/user.ts
+++ src/recoil/user.ts
@@ -1,11 +1,16 @@
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist();
 
 export const accessTokenState = atom<string | null>({
   key: 'accessTokenState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const refreshTokenState = atom<string | null>({
   key: 'refreshTokenState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
 });
