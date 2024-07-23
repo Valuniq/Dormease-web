8d4976fa9384f929bd604630677a0b2a03c069ab
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-07-23 15:44:07 +0900

    chore: point management list mock data

--- src/app/dashboard/StudentManagement/PointManagement/page.tsx
+++ src/app/dashboard/StudentManagement/PointManagement/page.tsx
@@ -5,32 +5,43 @@ import PointManagementTemplate from '@/components/templates/pointManagement/inde
 import { BASE_URL } from '@/constants/path';
 import InfiniteScroll from '@/hooks/useInfiniteScroll';
 import { PointMemberResponseDataList, PointListResponseInfo } from '@/types/pointManagement';
+import { useState, useEffect } from 'react';
 import { mutate } from 'swr';
+import { mockPointMemberResponse, mockPointListResponse } from './mockData';
 
 const PointManagementPage = () => {
-  const {
-    pointManagementData,
-    error: pointManagementError,
-    isLoadingMore,
-    size,
-    setSize,
-    isReachingEnd,
-  } = useInfinitePointMemberList();
-  const { data: pointData, error: pointError, isLoading: pointLoading } = usePointsDetail();
+  // const {
+  //   pointManagementData,
+  //   error: pointManagementError,
+  //   isLoadingMore,
+  //   size,
+  //   setSize,
+  //   isReachingEnd,
+  // } = useInfinitePointMemberList();
+  // const { data: pointData, error: pointError, isLoading: pointLoading } = usePointsDetail();
 
-  if (pointManagementError || pointError) {
-    console.error('Error fetching point management data:', pointManagementError);
-    console.error('Error fetching point data:', pointError);
-    return <div>Error loading data</div>;
-  }
+  // if (pointManagementError || pointError) {
+  //   console.error('Error fetching point management data:', pointManagementError);
+  //   console.error('Error fetching point data:', pointError);
+  //   return <div>Error loading data</div>;
+  // }
 
-  const pointLists: PointListResponseInfo[] = pointData?.information ?? [];
+  // const pointLists: PointListResponseInfo[] = pointData?.information ?? [];
 
-  // 데이터 갱신 시 SWR 캐시를 업데이트
-  const refreshData = () => {
-    mutate(`${BASE_URL}/api/v1/web/points`);
-    mutate(`${BASE_URL}/api/v1/web/points/detail`);
-  };
+  // // 데이터 갱신 시 SWR 캐시를 업데이트
+  // const refreshData = () => {
+  //   mutate(`${BASE_URL}/api/v1/web/points`);
+  //   mutate(`${BASE_URL}/api/v1/web/points/detail`);
+  // };
+
+  const [pointManagementData, setPointManagementData] = useState<PointMemberResponseDataList[]>([]);
+  const [pointLists, setPointLists] = useState<PointListResponseInfo[]>([]);
+
+  useEffect(() => {
+    // Simulate fetching data
+    setPointManagementData(mockPointMemberResponse.information.dataList);
+    setPointLists(mockPointListResponse.information);
+  }, []);
 
   return (
     <PointManagementTemplate
