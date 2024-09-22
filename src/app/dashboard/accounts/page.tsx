85379a7bd92683aeebc31618db095a9985b305f6
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-09-22 16:17:42 +0900

    feat: 보안 코드 api 연동

--- src/app/dashboard/accounts/page.tsx
+++ src/app/dashboard/accounts/page.tsx
@@ -1,15 +1,21 @@
 'use client';
 import { useAdminAccount } from '@/apis/account';
+import BackDrop from '@/components/organisms/BackDrop/Backdrop';
+import ConfirmPrompt from '@/components/organisms/Prompt/ConfirmPrompt/ConfirmPrompt';
 import DormeaseInfoBox from '@/components/templates/Accounts/DormeaseInfoBox';
 import DormInfoBox from '@/components/templates/Accounts/DormInfoBox';
 import ManagerNameBox from '@/components/templates/Accounts/ManagerNameBox';
 import ManagerPwBox from '@/components/templates/Accounts/ManagerPwBox';
-import { accountsActiveAreaState } from '@/recoil/account';
-import React from 'react';
+import SecureConfirmPrompt from '@/components/templates/Accounts/SecureConfirmPrompt';
+import { accountsActiveAreaState, accountsModalState } from '@/recoil/account';
+import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 const Page = () => {
+  const [secure, setSecure] = useState('');
   const [activeAreaState, setActiveAreaState] = useRecoilState(accountsActiveAreaState);
+  const [modalState, setModalState] = useRecoilState(accountsModalState);
+
   const { data: adminAccount, error, isLoading } = useAdminAccount();
 
   // NameBox 활성화 상태 변경
@@ -30,43 +36,79 @@ const Page = () => {
     }));
   };
 
+  // 모달 열기 함수
+  const openModal = (modalName: string) => {
+    setModalState((prevState) => ({
+      ...prevState,
+      [modalName]: true,
+    }));
+  };
+
+  // 모달 닫기 함수
+  const closeModal = (modalName: string) => {
+    setModalState((prevState) => ({
+      ...prevState,
+      [modalName]: false,
+    }));
+  };
   // DormInfoBox와 DormeaseInfoBox가 nameBox 또는 passwordBox가 편집 모드일 때 비활성화
   const dormInfoActive = !activeAreaState.namebox && !activeAreaState.passwordBox;
 
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <h1 className='H0 text-gray-grayscale50 mb-41'>계정 관리</h1>
-      <div
-        className={`w-[1079px] transition-all duration-500 ease-in-out h-594 shadow3 rounded-20 flex items-center justify-center gap-20 ${dormInfoActive ? 'bg-white' : 'bg-[rgba(0,0,0,0.25)]'}`}
-      >
-        <div className='flex flex-col items-center justify-center gap-20'>
-          <DormInfoBox
-            dormInfo={adminAccount?.information.schoolName || '관리자 학교 이름'}
-            isActive={dormInfoActive}
+    <>
+      {modalState.passwordSecure && (
+        <BackDrop isOpen={modalState.passwordSecure}>
+          <SecureConfirmPrompt
+            variant={'red'}
+            label={'비밀번호 변경을 위해 보안코드를 입력해주세요'}
+            onClose={() => closeModal('passwordSecure')}
+            secure={secure}
+            setSecure={setSecure}
+            activatePasswordEdit={togglePasswordBox}
           />
-          <div className='flex gap-20'>
-            <ManagerNameBox
-              id={adminAccount?.information.loginId || '관리자 아이디'}
-              name={adminAccount?.information.adminName || '관리자 이름'}
-              isActive={!activeAreaState.passwordBox} // PasswordBox가 편집 모드일 때 비활성화
-              isEditMode={activeAreaState.namebox} // nameBox 편집 모드 상태
-              setIsEditMode={toggleNameBox} // NameBox 편집 모드 토글 함수
+        </BackDrop>
+      )}
+
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='H0 text-gray-grayscale50 mb-41'>계정 관리</h1>
+        <div
+          className={`w-[1079px] transition-all duration-500 ease-in-out h-594 shadow3 rounded-20 flex items-center justify-center gap-20 ${dormInfoActive ? 'bg-white' : 'bg-[rgba(0,0,0,0.25)]'}`}
+        >
+          <div className='flex flex-col items-center justify-center gap-20'>
+            <DormInfoBox
+              dormInfo={adminAccount?.information.schoolName || '관리자 학교 이름'}
+              isActive={dormInfoActive}
             />
-            <ManagerPwBox
-              isActive={!activeAreaState.namebox} // NameBox가 편집 모드일 때 비활성화
-              isEditMode={activeAreaState.passwordBox} // passwordBox 편집 모드 상태
-              setIsEditMode={togglePasswordBox} // PasswordBox 편집 모드 토글 함수
-            />
+            <div className='flex gap-20'>
+              <ManagerNameBox
+                id={adminAccount?.information.loginId || '관리자 아이디'}
+                name={adminAccount?.information.adminName || '관리자 이름'}
+                isActive={!activeAreaState.passwordBox} // PasswordBox가 편집 모드일 때 비활성화
+                isEditMode={activeAreaState.namebox} // nameBox 편집 모드 상태
+                setIsEditMode={toggleNameBox} // NameBox 편집 모드 토글 함수
+              />
+              <ManagerPwBox
+                isActive={!activeAreaState.namebox} // NameBox가 편집 모드일 때 비활성화
+                isEditMode={activeAreaState.passwordBox} // passwordBox 편집 모드 상태
+                setIsEditMode={togglePasswordBox} // PasswordBox 편집 모드 토글 함수
+                setSecureMode={() => openModal('passwordSecure')}
+                openModal={openModal}
+                closeModal={closeModal}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-20 items-center justify-center'>
+            <DormeaseInfoBox address={''} tel={''} fax={''} isActive={dormInfoActive} />
+            <button
+              disabled={dormInfoActive}
+              className='w-333 h-57 rounded-10 bg-red-red10 text-white btn-cap flex items-center justify-center'
+            >
+              종료
+            </button>
           </div>
         </div>
-        <div className='flex flex-col gap-20 items-center justify-center'>
-          <DormeaseInfoBox address={''} tel={''} fax={''} isActive={dormInfoActive} />
-          <button className='w-333 h-57 rounded-10 bg-red-red10 text-white btn-cap flex items-center justify-center'>
-            종료
-          </button>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/templates/Accounts/SecureConfirmPrompt.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import PromptWarning from '@public/images/PromptWarning.png';
+import AlertBtn from '@/components/atoms/AllBtn/AlertBtn/AlertBtn';
+import PromptHeader from '@/components/atoms/Prompt/PromptHeader/PromptHeader';
+import { checkSecurityCode } from '@/apis/account';
+
+type Props = {
+  variant: 'blue' | 'red' | 'green';
+  label: string;
+  onClose: () => void;
+  secure: string;
+  setSecure: (secure: string) => void;
+  activatePasswordEdit: () => void;
+};
+
+const SecureConfirmPrompt = ({ variant, label, onClose, secure, setSecure, activatePasswordEdit }: Props) => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleConfirm = async () => {
+    try {
+      const data = await checkSecurityCode(secure);
+      if (data.check && data.information.checked) {
+        setSecure('');
+        activatePasswordEdit(); // 비밀번호 편집 모드 활성화
+        onClose();
+      } else {
+        throw new Error('보안코드가 일치하지 않습니다.');
+      }
+    } catch (error) {
+      console.error('보안코드 검증 실패:', error);
+      setErrorMessage('보안코드 검증에 실패했습니다.');
+    }
+  };
+
+  return (
+    <div className='w-456 h-239 flex flex-col items-center rounded-8 shadow-xl bg-white pb-15'>
+      <PromptHeader bgColor={'bg-red-red20'} onCancel={onClose} />
+      <Image src={PromptWarning} className='mt-19 mb-16' width={22} height={19} alt='PromptWarning' />
+      <h1 className='alert-cap text-gray-grayscale50 text-center mb-12'>
+        {label.split('\\n').map((line, index) => (
+          <p key={index} className='leading-25'>
+            {line}
+          </p>
+        ))}
+      </h1>
+
+      {/* 입력 필드 */}
+      <input
+        className={`w-235 border-[0.5px] outline-none px-8 rounded-8 text-gray-grayscale50 placeholder:text-gray-grayscale30 border-${
+          secure ? 'gray-grayscale50' : 'gray-grayscale30'
+        } H4-caption`}
+        placeholder='보안 코드를 입력하세요'
+        type='password'
+        value={secure}
+        onChange={(e) => setSecure(e.target.value)}
+      />
+
+      {/* 오류 메시지 표시 */}
+      {errorMessage && <p className='text-red-red20 caption-2 text-center mt-5'>{errorMessage}</p>}
+
+      <div className='ml-auto mt-auto'>
+        <AlertBtn label={'취소'} onClick={onClose} hoverColor={variant} />
+        <AlertBtn label={'확인'} onClick={handleConfirm} hoverColor={variant} />
+      </div>
+    </div>
+  );
+};
+
+export default SecureConfirmPrompt;
