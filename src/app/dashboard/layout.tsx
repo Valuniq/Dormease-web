2d1ff75b22c98f2d111bbd82924300fccf870973
rlotr02 <0314kelee@naver.com>
2024-09-25 09:20:45 +0900

    feat: UI 크기 조절

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         className={`flex align-start ml-19 ${pathname !== '/dashboard/schedules' && 'xxl:scale-80 xl:scale-65 lg:scale-65 md:scale-50 sm:scale-40'} transform-origin-top-left mt-132 xxl:mt-132 xl:mt-120 lg:mt-120 md:mt-100 sm:mt-80`}
       >
         <nav
-          className={`mr-101 ${pathname === '/dashboard/schedules' && 'transform-origin-top-left xxl:scale-80 xl:scale-65 lg:scale-65 md:scale-50 sm:scale-40'}`}
+          className={`${pathname === '/dashboard/schedules' ? 'transform-origin-top-left xxl:scale-80 xl:scale-65 lg:scale-65 md:scale-50 sm:scale-40 mr-50' : 'mr-101'}`}
         >
           <Navigation />
         </nav>
