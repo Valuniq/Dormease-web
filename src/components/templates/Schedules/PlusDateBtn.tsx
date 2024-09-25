0e603af82847e2b5a2b278ece44a21b0f072917e
rlotr02 <0314kelee@naver.com>
2024-09-25 09:49:51 +0900

    feat: height 추가

--- src/components/templates/Schedules/PlusDateBtn.tsx
+++ src/components/templates/Schedules/PlusDateBtn.tsx
@@ -4,12 +4,12 @@ import CircleArrowRight from '@public/images/CircleArrowRight.svg';
 
 const PlusDateBtn = ({ ...props }: React.HtmlHTMLAttributes<HTMLButtonElement>) => {
   return (
-    <button {...props} className='flex flex-col items-center w-80 pt-5 pb-10 bg-white'>
-      <PlusSquare width={20} />
+    <button {...props} className='flex flex-col items-center w-80 pt-5 pb-10'>
+      <PlusSquare width={20} height={20} />
       <h3 className='Caption2 text-15 text-blue-blue30 mt-5'>일정 추가</h3>
       <div className='flex gap-5'>
         <h3 className='Caption2 text-15 text-blue-blue30'>기간 선택</h3>
-        <CircleArrowRight width={17} />
+        <CircleArrowRight width={17} height={17} />
       </div>
     </button>
   );
