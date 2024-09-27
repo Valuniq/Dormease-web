c0b812a27329eefc80ac4bd4ad89dfab96918582
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-09-27 19:57:51 +0900

    feat: 이전 입사 신청 설정 목록 3개 불러오기 API 연동

--- src/apis/join.ts
+++ src/apis/join.ts
@@ -1,5 +1,10 @@
 import { BASE_URL } from '@/constants/path';
-import { joinDormitoriesResponse, joinDormitoriesResponseInformation } from '@/types/join';
+import {
+  joinDormitoriesResponse,
+  joinDormitoriesResponseInformation,
+  joinTopThreeListResponse,
+  joinTopThreeListResponseInformation,
+} from '@/types/join';
 import fetchWithTokens from '@/utils/fetchWithTokens';
 import useSWR from 'swr';
 
@@ -11,6 +16,20 @@ export const useGetJoinDormitories = () => {
 
   return {
     data: data?.information as joinDormitoriesResponseInformation[] | undefined,
+    error: error as any,
+    isLoading: !error && !data, // 데이터와 에러 둘 다 없으면 로딩 중으로 처리
+  };
+};
+
+// 이전 입사 신청 설정 목록 3개 조회
+export const useGetJoinThreeLists = () => {
+  const { data, error } = useSWR<joinTopThreeListResponse>(
+    `${BASE_URL}/api/v1/web/dormitoryApplicationSetting/topThree`,
+    fetchWithTokens,
+  );
+  console.log(data);
+  return {
+    data: data?.information as joinTopThreeListResponseInformation[] | undefined,
     error: error as any,
     isLoading: !error && !data, // 데이터와 에러 둘 다 없으면 로딩 중으로 처리
   };
--- src/types/join.ts
+++ src/types/join.ts
@@ -7,5 +7,17 @@ export type joinDormitoriesResponseInformation = {
   dormitoryRoomTypeId: number;
   dormitoryName: string;
   roomSize: number;
-  gender: 'MALE' | 'FEMAIL' | 'EMPTY';
+  gender: 'MALE' | 'FEMALE' | 'EMPTY';
+};
+
+export type joinTopThreeListResponse = {
+  check: boolean;
+  information: joinTopThreeListResponseInformation[];
+};
+
+export type joinTopThreeListResponseInformation = {
+  dormitoryApplicationSettingId: number;
+  title: string;
+  startDate: string;
+  endDate: string;
 };
