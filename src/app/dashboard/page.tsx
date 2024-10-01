24fa9986954d545613bb4646c0f09c60500bac64
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-02 06:57:58 +0900

    feat: 메인 화면 레이아웃 설정

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,29 @@
-'use client';
 import React from 'react';
+import Shortcut from '@/components/templates/Main/Shortcut/Shortcut';
+import Schedule from '@/components/templates/Main/Schedule/Schedule';
+import Request from '@/components/templates/Main/Request/Request';
+import Refund from '@/components/templates/Main/Refund/Refund';
+import Notice from '@/components/templates/Main/Notice/Notice';
 
 const Page = () => {
-  return <div>Main Page</div>;
+  return (
+    <div className='w-full'>
+      <h1 className='font-pretendard font-bold text-45 text-blue-blue30 mb-27'>메인 화면</h1>
+      <div className='flex items-start gap-63'>
+        <div className='flex flex-col gap-25 justify-center'>
+          <Shortcut />
+          <div className='flex gap-25 items-center'>
+            <Request />
+            <Refund />
+          </div>
+        </div>
+        <div className='flex flex-col items-center gap-25'>
+          <Schedule />
+          <Notice />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
