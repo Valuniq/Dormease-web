ae0384e2799b31ba25988b53fe35ebbfa397710d
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-01 18:58:38 +0900

    feat: 입사 신청 설정 작성 Alert, 비어 있음 Alert 모달 창 띄우기

--- src/recoil/join.ts
+++ src/recoil/join.ts
@@ -1,10 +1,38 @@
-import { joinDormitoriesResponseInformation, joinPostRequestTermReqList } from '@/types/join';
+import { joinDormitoriesResponseInformation, joinPostRequest, joinPostRequestTermReqList } from '@/types/join';
 import { atom } from 'recoil';
 
 // 기숙사 정보 상태
 export const dormitoryRoomTypeState = atom<joinDormitoriesResponseInformation[]>({
   key: 'dormitoryRoomTypeState',
   default: [], // 기숙사 정보를 불러온 후 동적으로 입력 필드 생성
+});
+
+// 통합 상태: 입사 신청 설정 정보
+export const joinApplicationState = atom<joinPostRequest>({
+  key: 'joinApplicationState',
+  default: {
+    schoolId: 1, // 예시로 넣은 값
+    title: '',
+    startDate: '',
+    endDate: '',
+    depositStartDate: '',
+    depositEndDate: '',
+    securityDepoist: null,
+    dormitoryRoomTypeReqList: [
+      { dormitoryRoomTypeId: 0, acceptLimit: null }, // 기본값 설정
+    ],
+    termReqList: [
+      {
+        termName: '',
+        startDate: '',
+        endDate: '',
+        dormitoryTermReqList: [
+          { dormitoryRoomTypeId: 0, price: null }, // 기본값 설정
+        ],
+      },
+    ],
+    mealTicketReqList: [],
+  },
 });
 
 // 기간별로 금액 입력 필드를 관리하기 위한 상태
@@ -61,4 +89,22 @@ export const termReqListState = atom<joinPostRequestTermReqList[]>({
 export const termReqIsActiveState = atom<boolean[]>({
   key: 'isActiveState',
   default: [true, false, false, false], // 각 기간의 활성화 상태
+});
+
+// 비활성화 상태를 저장
+export const disabledFieldsState = atom<Record<number, boolean>>({
+  key: 'disabledFieldsState',
+  default: {},
+});
+
+export type JoinModalState = {
+  [key: string]: boolean;
+};
+
+export const joinModalState = atom<JoinModalState>({
+  key: 'joinModalState',
+  default: {
+    isEmptyAlert: false,
+    isPostChecked: false,
+  },
 });
