513267d09e716a5cd5a8a5ee18d713e0e3b9a9bb
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-02 10:35:55 +0900

    feat: 기본 날짜 설정 업데이트 및 utils 생성

--- src/components/templates/Join/Default/Default.tsx
+++ src/components/templates/Join/Default/Default.tsx
@@ -1,30 +1,56 @@
+import { joinApplicationState } from '@/recoil/join';
 import React from 'react';
+import { useRecoilState } from 'recoil';
 import Deposit from './Deposit';
 import Period from './Period';
 
 const Default = () => {
+  const [applicationData, setApplicationData] = useRecoilState(joinApplicationState);
+
+  // 입사 신청 기간 업데이트 함수
+  const updateApplicationPeriod = (newPeriod: { startDate: string; endDate: string }) => {
+    setApplicationData({
+      ...applicationData,
+      startDate: newPeriod.startDate,
+      endDate: newPeriod.endDate,
+    });
+  };
+
+  // 입금 가능 기간 업데이트 함수
+  const updateDepositPeriod = (newDepositPeriod: { startDate: string; endDate: string }) => {
+    setApplicationData({
+      ...applicationData,
+      depositStartDate: newDepositPeriod.startDate,
+      depositEndDate: newDepositPeriod.endDate,
+    });
+  };
+
+  // 보증금 업데이트 함수
+  const updateSecurityDeposit = (newDeposit: number) => {
+    setApplicationData({
+      ...applicationData,
+      securityDepoist: newDeposit,
+    });
+  };
+
   return (
     <div className='px-8 flex items-center justify-between border-t-gray-grayscale50 border-t-1 py-20'>
+      {/* 입사 신청 기간 */}
       <Period
         label={'입사 신청 기간'}
-        input={''}
-        setInput={function (input: string): void {
-          throw new Error('Function not implemented.');
-        }}
+        input={{ startDate: applicationData.startDate, endDate: applicationData.endDate }}
+        setInput={updateApplicationPeriod}
       />
+
+      {/* 입금 가능 기간 */}
       <Period
         label={'입금 가능 기간'}
-        input={''}
-        setInput={function (input: string): void {
-          throw new Error('Function not implemented.');
-        }}
+        input={{ startDate: applicationData.depositStartDate, endDate: applicationData.depositEndDate }}
+        setInput={updateDepositPeriod}
       />
-      <Deposit
-        input={''}
-        setInput={function (input: string): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
+
+      {/* 보증금 */}
+      <Deposit input={applicationData.securityDepoist} setInput={updateSecurityDeposit} />
     </div>
   );
 };
--- src/components/templates/Join/Default/Deposit.tsx
+++ src/components/templates/Join/Default/Deposit.tsx
@@ -1,16 +1,23 @@
-import TextBoxes from '@/components/atoms/InputText/JoinSettingEntryTextBoxes/TextBoxes';
+'use client';
+
 import React from 'react';
 
 type Props = {
-  input: string;
-  setInput: (input: string) => void;
+  input: number | null;
+  setInput: (input: number) => void;
 };
 
 const DepositSetting = ({ input, setInput }: Props) => {
   return (
     <div className='flex items-center justify-between H4 text-gray-grayscale50'>
       <h1 className='mr-31'> 보증금 </h1>
-      <TextBoxes input={input} setInput={setInput} placeholder={'금액 입력'} type={'textBox6'} />
+      <input
+        type='number'
+        value={input !== null ? input : ''}
+        onChange={(e) => setInput(parseInt(e.target.value))}
+        className={`${input ? 'border-gray-grayscale50' : 'border-gray-grayscale30'} text-right H4-caption border-[0.5px] outline-none px-8 w-145 rounded-8 text-gray-grayscale50 placeholder:text-gray-grayscale30`}
+        placeholder='금액 입력'
+      />
       <div className='w-3' />원
     </div>
   );
