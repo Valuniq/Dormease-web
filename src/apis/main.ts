8da596a055fb6ae372e19924db086cd5182956d6
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-02 12:40:25 +0900

    feat: requests 조회 api 연동

---
+++ src/apis/main.ts
@@ -0,0 +1,28 @@
+import { BASE_URL } from '@/constants/path';
+import { RefundResponse, NoticeResponse, RequestResponse, TodayScheduleResponse } from '@/types/main';
+import fetchWithTokens from '@/utils/fetchWithTokens';
+import useSWR from 'swr';
+
+// 환불 신청 사생 목록 조회
+export const useMainRefund = () => {
+  const { data, error } = useSWR<RefundResponse>(`${BASE_URL}/api/v1/web/refundRequestment/main`, fetchWithTokens);
+  return { data, error, isLoading: !error && !data };
+};
+
+// 공지사항 목록 조회
+export const useMainNotice = () => {
+  const { data, error } = useSWR<NoticeResponse>(`${BASE_URL}/api/v1/app/notifications`, fetchWithTokens);
+  return { data, error, isLoading: !error && !data };
+};
+
+// 요청사항 목록 조회
+export const useMainRequest = () => {
+  const { data, error } = useSWR<RequestResponse>(`${BASE_URL}/api/v1/web/requestments/main`, fetchWithTokens);
+  return { data, error, isLoading: !error && !data };
+};
+
+// 오늘의 일정 목록 조회
+export const useMainTodaySchedule = () => {
+  const { data, error } = useSWR<TodayScheduleResponse>(`${BASE_URL}/api/v1/web/calendar/today`, fetchWithTokens);
+  return { data, error, isLoading: !error && !data };
+};
--- src/components/templates/Main/Request/Request.tsx
+++ src/components/templates/Main/Request/Request.tsx
@@ -1,63 +1,45 @@
-import { requests } from '@/constants/navigation';
+'use client';
+import { useMainRequest } from '@/apis/main';
+import { requests, requestsDetail } from '@/constants/navigation';
+import { requestIdState } from '@/recoil/request';
 import Link from 'next/link';
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 import RequestList from './RequestList';
 
 const Request = () => {
+  const { data, isLoading, error } = useMainRequest();
+  const setRequestId = useSetRecoilState(requestIdState);
+
   return (
     <div className='font-pretendard w-514 h-574'>
       <div className='flex items-center justify-between mb-3'>
-        <span className='font-semibold text-24 text-gray-grayscale40 pl-15'>오늘의 일정</span>
+        <span className='font-semibold text-24 text-gray-grayscale40 pl-15'>요청사항</span>
         <Link href={requests} passHref>
           <span className='font-normal text-blue-blue40 text-17 underline'>바로가기</span>
         </Link>
       </div>
-      <div className='w-full bg-white h-560 rounded-30  overflow-y-scroll'>
-        {mockupData.map((i, index) => (
-          <Link key={index} href={'/'} passHref>
-            <ul className='mb-20 flex justify-center '>
-              <RequestList content={i.content} date={i.date} />
-            </ul>
-          </Link>
-        ))}
+      <div className='w-full flex bg-white h-560 rounded-30 overflow-y-scroll'>
+        {isLoading ? (
+          <div className='H2 text-gray-grayscale30 my-auto mx-auto'>로딩 중...</div>
+        ) : error ? (
+          <div className='H2 text-red-red30  my-auto mx-auto'>에러가 발생했습니다. 다시 시도해주세요.</div>
+        ) : !data || data.information.length === 0 ? (
+          <div className='H2 text-gray-grayscale30  my-auto mx-auto'>아직 요청사항이 없습니다.</div>
+        ) : (
+          <div>
+            {data.information.map((i, index) => (
+              <Link key={index} href={requestsDetail} passHref>
+                <ul className='mb-20 flex justify-center' onClick={() => setRequestId(i.requestmentId)}>
+                  <RequestList content={i.title} date={i.createdDate} />
+                </ul>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default Request;
-const mockupData = [
-  {
-    content: '안녕하세요 도미동 202호에서 거주하고 있는데 여자 화장실 왼쪽에서 3번째 칸의 문이 어제부터 잠겨있습니다.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content: '안녕하세요 얼굴 등록을 했는데, 출입이 인식을 잘 못하는 것 같습니다. 얼굴 재 등록을 해주셨으면 합니다.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content:
-      '안녕하세요 사감님께 말씀드렸는데, 사무실에 연락드리라고 하셔서 연락드립니다. 왼쪽에서 두번째 세탁기가 고장났어요.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content:
-      '안녕하세요 사감님께 말씀드렸는데, 사무실에 연락드리라고 하셔서 연락드립니다. 왼쪽에서 두번째 세탁기가 고장났어요.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content:
-      '안녕하세요 사감님께 말씀드렸는데, 사무실에 연락드리라고 하셔서 연락드립니다. 왼쪽에서 두번째 세탁기가 고장났어요.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content:
-      '안녕하세요 사감님께 말씀드렸는데, 사무실에 연락드리라고 하셔서 연락드립니다. 왼쪽에서 두번째 세탁기가 고장났어요.',
-    date: '2024년 6월 15일',
-  },
-  {
-    content:
-      '안녕하세요 사감님께 말씀드렸는데, 사무실에 연락드리라고 하셔서 연락드립니다. 왼쪽에서 두번째 세탁기가 고장났어요.',
-    date: '2024년 6월 15일',
-  },
-];
