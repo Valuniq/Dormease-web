caf6277af8349f045dac04e44881bf9ebdaa5786
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-04 22:49:29 +0900

    feat: Notice Writing, Edit 별도 반응형 추가

--- src/components/atoms/InputText/LongInputText/LongInputText.tsx
+++ src/components/atoms/InputText/LongInputText/LongInputText.tsx
@@ -1,17 +1,20 @@
 type Props = {
+  width?: string;
+  height?: string;
+  font?: string;
   placeholder: string;
   input: string;
   setInput: (input: string) => void;
 };
 
-const LongInputText = ({ placeholder, input, setInput }: Props) => {
+const LongInputText = ({ placeholder, font, input, setInput, width, height }: Props) => {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
   return (
     <input
-      className={`${input ? 'border-gray-grayscale50 ' : 'border-gray-grayscale20'} H4 px-6 rounded-5 focus:border-gray-grayscale50 w-924 h-34 border-1 placeholder:text-gray-grayscale20 focus:outline-none text-gray-grayscale50 `}
+      className={`${input ? 'border-gray-grayscale50 ' : 'border-gray-grayscale20'} ${width || 'w-924'} ${height || 'h-34'} ${font ? font : 'H4'} px-6 rounded-5 focus:border-gray-grayscale50  border-1 placeholder:text-gray-grayscale20 focus:outline-none text-gray-grayscale50 `}
       placeholder={placeholder}
       type='text'
       value={input}
--- src/components/templates/Notice/Writing/NoticeWriting.tsx
+++ src/components/templates/Notice/Writing/NoticeWriting.tsx
@@ -45,29 +45,38 @@ const NoticeWriting = forwardRef<HTMLDivElement, Props>(
     const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     return (
-      <div ref={ref} className='w-[1200px] px-12'>
-        <div className='flex items-center py-30 border-t-1 border-t-gray-grayscale50'>
-          <div className='flex items-center'>
-            <h1 className='mr-40 text-nowrap H4 text-gray-grayscale50'>제 목</h1>
-            <LongInputText placeholder={'제목을 입력해주세요.'} input={title} setInput={setTitle} />
+      <div ref={ref} className='xxl:w-full xl:w-800 lg:w-700 md:w-600 w-full  ml-[-80px]'>
+        <div className='w-full flex items-center justify-between py-15 border-t-1 border-t-gray-grayscale50'>
+          <div className='w-full flex items-center justify-between'>
+            <div className=' flex-1 flex items-center mr-20'>
+              <h1 className='mr-20 text-nowrap font-medium text-gray-grayscale50'>제 목</h1>
+              <LongInputText
+                width={'flex-1'}
+                height={'h-30'}
+                font={'text-medium'}
+                placeholder={'제목을 입력해주세요.'}
+                input={title}
+                setInput={setTitle}
+              />
+            </div>
+            <div className='flex-2 flex items-center bg-blue-blue15'>
+              <Checkbox isChecked={isPinned} setIsChecked={setIsPinned} />
+              <h2 className='text-nowrap mx-18  text-gray-grayscale50'>최상단 고정</h2>
+              <Image src={pinIcon} width={13} height={20} className='object-contain rotate-[-45deg]' alt='pin' />
+            </div>
           </div>
-          <div className='ml-19 flex items-center'>
-            <Checkbox isChecked={isPinned} setIsChecked={setIsPinned} />
-            <h2 className='text-nowrap mx-18 H4-caption text-gray-grayscale50'>최상단 고정</h2>
-            <Image src={pinIcon} width={13.35} height={20.14} className='object-contain rotate-[-45deg]' alt='pin' />
-          </div>
         </div>
-        <div className='flex items-center py-13 border-y-1 border-y-gray-grayscale30'>
-          <h1 className='mr-40 text-nowrap H4 text-gray-grayscale50'>작성자</h1>
-          <h1 className='text-nowrap H4 text-gray-grayscale40'>{writer}</h1>
+        <div className='flex items-center py-15 border-y-1 border-y-gray-grayscale30'>
+          <h1 className='mr-20 text-nowrap font-medium text-gray-grayscale50'>작성자</h1>
+          <h1 className='text-nowrap font-medium text-gray-grayscale40'>{writer}</h1>
         </div>
-        <div className='flex items-center py-23 mb-50'>
-          <h1 className=' h-430 items-center flex mr-51 text-nowrap H4 text-gray-grayscale50'>내 용</h1>
-          <QuillComponent width='100%' height='430px' setEditorHtml={setEditorHtml} />
+        <div className='flex w-full items-center justify-center pt-15 mb-60'>
+          <h1 className=' h-430 items-center flex mr-51 text-nowrap font-medium text-gray-grayscale50'>내 용</h1>
+          <QuillComponent width='100%' height='70svh' setEditorHtml={setEditorHtml} />
         </div>
-        <div className='flex items-center py-23 border-y-1 border-y-gray-grayscale30'>
-          <h1 className='H4 text-gray-grayscale50 mr-20 text-nowrap'>첨부파일</h1>
-          <CheckFileBtn label={'파일선택'} onClick={() => fileInputRef.current?.click()} />
+        <div className='flex items-center py-15 border-y-1 border-y-gray-grayscale30'>
+          <h1 className='font-medium text-gray-grayscale50 mr-20 text-nowrap'>첨부파일</h1>
+          <CheckFileBtn font={'font-medium text-13'} label={'파일선택'} onClick={() => fileInputRef.current?.click()} />
           <input type='file' ref={fileInputRef} className='hidden' multiple onChange={handleFileChange} />
           <div className='overflow-scroll noti-file-scrollbar  ml-20 w-[917px] h-34 rounded-5 flex items-center justify-start px-10 border-1 border-gray-grayscale30'>
             {fileLists.map((file, index) => (
