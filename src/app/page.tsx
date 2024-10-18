af2c50b7ecaa5c95a589ae9b9a1c86a119263e54
LEE BO HYUN <92661884+frombozztoang@users.noreply.github.com>
2024-10-18 18:59:59 +0900

    feat: 자동 로그인 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,140 +1,141 @@
 'use client';
-import { putAdminPassword } from '@/apis/account';
-import { signIn } from '@/apis/auth';
-import BackDrop from '@/components/organisms/BackDrop/Backdrop';
+
+import { reissueToken, signIn } from '@/apis/auth';
 import LoginForm from '@/components/organisms/LoginForm/LoginForm';
-import AlertPrompt from '@/components/organisms/Prompt/AlertPrompt/AlertPrompt';
-import ConfirmPrompt from '@/components/organisms/Prompt/ConfirmPrompt/ConfirmPrompt';
-import { POL_ACCOUNTS } from '@/constants/policy';
 import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants/tokenKey';
-import { accountsModalState } from '@/recoil/account';
 import { accessTokenState, refreshTokenState } from '@/recoil/user';
 import tokenManager from '@/utils/tokenManager';
-import { validatePassword } from '@/utils/validatePassword';
 import LoginBackgroundImg from '@public/images/LoginBackgroundImg.png';
 import LoginLogo from '@public/logo/LoginLogo.png';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 const Page = () => {
   const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
-  const [newPassword, setNewPassword] = useState(''); // 새 비밀번호
-  const [confirmPassword, setConfirmPassword] = useState(''); // 새 비밀번호 확인
-  const [errorMessage, setErrorMessage] = useState('');
-  const [modalState, setModalState] = useRecoilState(accountsModalState);
-  const [isPWEditMode, setIsPWEditMode] = useState(false);
+  const [originalLoginId, setOriginalLoginId] = useState('');
+  const [originalPassword, setOriginalPassword] = useState('');
+  const [isAutoLogin, setIsAutoLogin] = useState(false); // 자동 로그인 상태 관리
+  const [autoLoginButtonLabel, setAutoLoginButtonLabel] = useState('로그인'); // 버튼 라벨 관리
 
   const isDisabled = !loginId || !password;
   const router = useRouter();
   const setAccessToken = useSetRecoilState(accessTokenState);
   const setRefreshToken = useSetRecoilState(refreshTokenState);
 
+  // 로그인 함수 (액세스 토큰, 리프레시 토큰, 아이디, 자동 로그인 여부 저장)
   const handleLogin = async ({ loginId, password }: { loginId: string; password: string }) => {
     try {
       const response = await signIn({ loginId, password });
 
-      if (response) {
-        const accessToken = response.information.accessToken;
-        const refreshToken = response.information.refreshToken;
+      const accessToken = response.information.accessToken;
+      const refreshToken = response.information.refreshToken;
 
-        // Recoil 상태 업데이트
-        setAccessToken(accessToken);
-        setRefreshToken(refreshToken);
+      // Recoil 상태 업데이트
+      setAccessToken(accessToken);
+      setRefreshToken(refreshToken);
 
-        // 로컬 스토리지에 토큰 저장
-        tokenManager.saveToken(ACCESS_TOKEN, accessToken);
-        tokenManager.saveToken(REFRESH_TOKEN, refreshToken);
+      // 로컬 스토리지에 토큰 저장
+      tokenManager.saveToken(ACCESS_TOKEN, accessToken);
+      tokenManager.saveToken(REFRESH_TOKEN, refreshToken);
 
-        // 상태 및 로컬 스토리지 확인
-        console.log('Access Token (Recoil):', accessToken);
-        console.log('Refresh Token (Recoil):', refreshToken);
-        console.log('Access Token (LocalStorage):', tokenManager.getToken(ACCESS_TOKEN));
-        console.log('Refresh Token (LocalStorage):', tokenManager.getToken(REFRESH_TOKEN));
-
-        alert('로그인 성공');
-        router.push('/dashboard');
+      // 자동 로그인이 체크된 경우 자동 로그인 상태, ID 저장
+      if (isAutoLogin) {
+        localStorage.setItem('loginId', loginId);
+        localStorage.setItem('isAutoLogin', 'true');
       } else {
-        alert('로그인 실패');
+        localStorage.removeItem('loginId');
+        localStorage.removeItem('isAutoLogin');
       }
+
+      router.push('/dashboard');
     } catch (error) {
       console.error(error);
+      console.error('로그인 중 오류가 발생했습니다.', error);
       alert('로그인 중 오류가 발생했습니다.');
     }
   };
 
-  useEffect(() => {
-    if (newPassword && !validatePassword(newPassword)) {
-      setErrorMessage(POL_ACCOUNTS.password.messages.invalid);
-    } else if (newPassword && confirmPassword && newPassword !== confirmPassword) {
-      setErrorMessage(POL_ACCOUNTS.password.messages.mismatch);
+  // 자동 로그인 함수 (리프레시 토큰 재발급, 실패 시 로그인 정보 삭제)
+  const handleAutoLogin = async () => {
+    const refreshToken = tokenManager.getToken(REFRESH_TOKEN);
+
+    if (refreshToken) {
+      try {
+        const newAccessToken = await reissueToken(refreshToken);
+
+        if (newAccessToken) {
+          setAccessToken(newAccessToken);
+          await router.push('/dashboard');
+        } else {
+          console.error('새로운 액세스 토큰 발급 실패');
+        }
+      } catch (error) {
+        alert('자동 로그인에 실패하였습니다. 다시 로그인해 주세요.');
+        // 자동 로그인 관련 저장된 정보 삭제
+        localStorage.removeItem('loginId');
+        localStorage.removeItem('isAutoLogin');
+        tokenManager.removeToken(REFRESH_TOKEN);
+        tokenManager.removeToken(ACCESS_TOKEN);
+        setIsAutoLogin(false);
+        setAutoLoginButtonLabel('로그인');
+      }
     } else {
-      setErrorMessage('');
+      console.log('리프레시 토큰 없음');
     }
-  }, [newPassword, confirmPassword]);
-
-  // 모달 열기 함수
-  const openModal = (modalName: string) => {
-    setModalState((prevState) => ({
-      ...prevState,
-      [modalName]: true,
-    }));
   };
 
-  // 모달 닫기 함수
-  const closeModal = (modalName: string) => {
-    setModalState((prevState) => ({
-      ...prevState,
-      [modalName]: false,
-    }));
+  // 자동 로그인 활성화, 저장된 아이디, 리프레시 토큰이 있으면 자동 로그인 모드 전환
+  useEffect(() => {
+    const savedAutoLogin = localStorage.getItem('isAutoLogin');
+    const savedLoginId = localStorage.getItem('loginId');
+    const refreshToken = tokenManager.getToken(REFRESH_TOKEN);
+
+    if (savedAutoLogin === 'true' && savedLoginId && refreshToken) {
+      setIsAutoLogin(true);
+      setLoginId(savedLoginId);
+      setOriginalLoginId(savedLoginId);
+      setPassword('******'); // 더미 텍스트 설정
+      setOriginalPassword('******');
+      setAutoLoginButtonLabel('자동 로그인');
+    }
+  }, []);
+
+  // 로그인 입력이 변경되면 자동 로그인 비활성화
+  const handleInputChange = (newLoginId: string, newPassword: string) => {
+    setLoginId(newLoginId);
+    setPassword(newPassword);
+    if (newLoginId !== originalLoginId || newPassword !== originalPassword) {
+      setAutoLoginButtonLabel('로그인');
+    }
   };
 
-  const handlePasswordChange = async () => {
-    try {
-      await putAdminPassword(newPassword);
-      closeModal('passwordConfirm');
-      openModal('passwordChanged');
-    } catch (error) {
-      closeModal('passwordConfirm');
-      openModal('passwordFailed');
+  // 자동 로그인 상태 변경 시 모드 전환
+  const handleAutoLoginToggle = (checked: boolean) => {
+    setIsAutoLogin(checked);
+    if (!checked) {
+      // 자동 로그인이 해제되면 라벨을 "로그인"으로 변경하고 ID와 비밀번호 초기화
+      setAutoLoginButtonLabel('로그인');
+      setLoginId(''); // ID 초기화
+      setPassword(''); // 비밀번호 초기화
+    }
+  };
+
+  // 모드에 따른 로그인 버튼 클릭 처리
+  const handleButtonClick = () => {
+    if (isAutoLogin && autoLoginButtonLabel === '자동 로그인') {
+      console.log('자동 로그인 시도');
+      handleAutoLogin();
+    } else {
+      console.log('일반 로그인 시도');
+      handleLogin({ loginId, password });
     }
   };
 
   return (
     <>
-      {modalState.passwordConfirm && (
-        <BackDrop isOpen={modalState.passwordConfirm}>
-          <ConfirmPrompt
-            variant={'blue'}
-            label={'비밀번호를 변경하시겠습니까?'}
-            onConfirm={handlePasswordChange}
-            onCancel={() => closeModal('passwordConfirm')}
-          />
-        </BackDrop>
-      )}
-      {modalState.passwordChanged && (
-        <BackDrop isOpen={modalState.passwordChanged}>
-          <AlertPrompt
-            variant={'blue'}
-            label={'비밀번호를 성공적으로 변경하였습니다.'}
-            onConfirm={() => {
-              closeModal('passwordChanged'), setNewPassword('');
-              setConfirmPassword('');
-            }}
-          />
-        </BackDrop>
-      )}
-      {modalState.passwordFailed && (
-        <BackDrop isOpen={modalState.passwordFailed}>
-          <AlertPrompt
-            variant={'red'}
-            label={'비밀번호 변경에 실패했습니다. 다시 시도해주세요.'}
-            onConfirm={() => closeModal('passwordFailed')}
-          />
-        </BackDrop>
-      )}
       <div
         style={{
           backgroundImage: `url(${LoginBackgroundImg.src})`,
@@ -149,18 +150,15 @@ const Page = () => {
           <Image src={LoginLogo} width={372} height={136.5} className='object-cover mb-43' alt='login logo' />
           <LoginForm
             loginId={loginId}
-            setLoginId={setLoginId}
+            setLoginId={(id) => handleInputChange(id, password)}
             password={password}
-            setPassword={setPassword}
-            errorMessage={errorMessage}
-            newPassword={newPassword}
-            setNewPassword={setNewPassword}
-            confirmedPassword={confirmPassword}
-            setConfirmedPassword={setConfirmPassword}
-            handleLogin={handleLogin}
+            setPassword={(pw) => handleInputChange(loginId, pw)}
+            handleLogin={handleButtonClick}
             loginBtnDisabled={isDisabled}
-            isPWEditMode={isPWEditMode}
-            setIsPWEditMode={setIsPWEditMode}
+            isAutoLogin={isAutoLogin}
+            setIsAutoLogin={handleAutoLoginToggle}
+            buttonLabel={autoLoginButtonLabel}
+            onLoginClick={handleButtonClick}
           />
         </div>
       </div>
--- src/components/organisms/LoginForm/LoginForm.tsx
+++ src/components/organisms/LoginForm/LoginForm.tsx
@@ -1,28 +1,22 @@
-'use client';
-import Checkbox from '@/components/atoms/AllBtn/Checkbox/Checkbox';
+import React from 'react';
+import { accountsModalState } from '@/recoil/account';
+import { useRecoilState } from 'recoil';
+
 import LoginBtn from '@/components/atoms/AllBtn/LoginBtn/LoginBtn';
 import LoginInputText from '@/components/atoms/InputText/LoginInputText/LoginInputText';
-import SecureConfirmPrompt from '@/components/templates/Accounts/SecureConfirmPrompt';
-import { accountsModalState } from '@/recoil/account';
-import { UserLoginRequest } from '@/types/user';
-import React, { useState } from 'react';
-import { useRecoilState } from 'recoil';
-import BackDrop from '../BackDrop/Backdrop';
+import Checkbox from '@/components/atoms/AllBtn/Checkbox/Checkbox';
 
 type Props = {
   loginId: string;
   setLoginId: (id: string) => void;
   password: string;
   setPassword: (pw: string) => void;
-  handleLogin: ({ loginId, password }: UserLoginRequest) => void;
+  handleLogin: ({ loginId, password }: { loginId: string; password: string }) => void;
   loginBtnDisabled: boolean;
-  errorMessage: string;
-  newPassword: string;
-  setNewPassword: (value: string) => void;
-  confirmedPassword: string;
-  setConfirmedPassword: (value: string) => void;
-  isPWEditMode: boolean;
-  setIsPWEditMode: (value: boolean) => void;
+  buttonLabel: string;
+  isAutoLogin: boolean;
+  setIsAutoLogin: (value: boolean) => void;
+  onLoginClick: () => void;
 };
 
 const LoginForm = ({
@@ -32,107 +26,34 @@ const LoginForm = ({
   setPassword,
   handleLogin,
   loginBtnDisabled,
-  newPassword,
-  setNewPassword,
-  confirmedPassword,
-  setConfirmedPassword,
-  errorMessage,
-  isPWEditMode,
-  setIsPWEditMode,
+  buttonLabel,
+  isAutoLogin,
+  setIsAutoLogin,
+  onLoginClick,
 }: Props) => {
-  const [secure, setSecure] = useState('');
   const [modalState, setModalState] = useRecoilState(accountsModalState);
-  // 비밀번호 편집 모드 활성화 함수
-  const activatePasswordEdit = () => {
-    setIsPWEditMode(true); // 비밀번호 편집 모드를 활성화
-  };
+
   return (
     <>
-      {modalState.passwordSecure && (
-        <BackDrop isOpen={modalState.passwordSecure}>
-          <SecureConfirmPrompt
-            variant={'red'}
-            label={'비밀번호 변경을 위해 보안코드를 입력해주세요'}
-            onClose={() => {
-              setModalState((prev) => ({ ...prev, passwordSecure: false })), setSecure('');
-            }}
-            secure={secure}
-            setSecure={setSecure}
-            activatePasswordEdit={activatePasswordEdit}
-          />
-        </BackDrop>
-      )}
-
       <div className='w-510 h-485 bg-white rounded-[12.35px] shadow-lg flex flex-col items-center justify-center'>
-        {isPWEditMode ? (
-          <>
-            <h1 className='H1 text-gray-grayscale50 mb-30'>비밀번호 재설정</h1>
-            <div>
-              <div className='mb-19'>
-                <LoginInputText
-                  input={newPassword}
-                  setInput={setNewPassword}
-                  placeholder={'새 비밀번호'}
-                  type={'password'}
-                />
-              </div>
-              <div className='mb-19'>
-                <LoginInputText
-                  input={confirmedPassword}
-                  setInput={setConfirmedPassword}
-                  placeholder={'새 비밀번호 확인'}
-                  type={'password'}
-                />
-              </div>
-              <div className='flex mx-auto mb-15 w-356 caption-1 text-gray-grayscale40 text-center'>
-                영문 대문자와 소문자, 숫자, 특수문자 중 2가지 이상을 조합하여 6~20자로 입력해주세요.
-              </div>
-
-              <LoginBtn
-                onClick={() => setModalState((prev) => ({ ...prev, passwordConfirm: true }))}
-                label={'비밀번호 변경'}
-                disabled={!(newPassword && confirmedPassword && !errorMessage)}
-              />
-              <div className='H4 text-red-red20 flex justify-center mt-10 items-center text-center'>{errorMessage}</div>
+        <>
+          <h1 className='H1 text-gray-grayscale50 mb-31'>도미즈 관리자 로그인</h1>
+          <div>
+            <div className='mb-19'>
+              <LoginInputText input={loginId} setInput={setLoginId} placeholder={'아이디'} type={'text'} />
             </div>
-          </>
-        ) : (
-          <>
-            {' '}
-            <h1 className='H1 text-gray-grayscale50 mb-31'>도미즈 관리자 로그인</h1>
-            <div>
-              <div className='mb-19'>
-                <LoginInputText input={loginId} setInput={setLoginId} placeholder={'아이디'} type={'text'} />
-              </div>
-              <div className='mb-19'>
-                <LoginInputText input={password} setInput={setPassword} placeholder={'비밀번호'} type={'password'} />
-              </div>
-              <div className='flex justify-between items-center  mb-27 px-5'>
-                <div className='flex items-center gap-8 '>
-                  <Checkbox
-                    isChecked={false}
-                    setIsChecked={function (isChecked: boolean): void {
-                      throw new Error('Function not implemented.');
-                    }}
-                  />
-                  <span className='alert-cap text-black'>자동 로그인</span>
-                </div>
-                <div
-                  onClick={() => setModalState((prev) => ({ ...prev, passwordSecure: true }))}
-                  className='cursor-pointer H4 text-blue-blue30'
-                >
-                  비밀번호 찾기
-                </div>
-              </div>
-
-              <LoginBtn
-                onClick={() => handleLogin({ loginId, password })}
-                label={'로그인'}
-                disabled={loginBtnDisabled}
-              />
+            <div className='mb-19'>
+              <LoginInputText input={password} setInput={setPassword} placeholder={'비밀번호'} type={'password'} />
             </div>
-          </>
-        )}
+            <div className='flex justify-between items-center mb-27 px-5'>
+              <div className='flex items-center gap-8'>
+                <Checkbox isChecked={isAutoLogin} setIsChecked={(checked) => setIsAutoLogin(checked)} />
+                <span className='alert-cap text-black'>자동 로그인</span>
+              </div>
+            </div>
+            <LoginBtn onClick={onLoginClick} label={buttonLabel} disabled={loginBtnDisabled} />
+          </div>
+        </>
       </div>
     </>
   );
